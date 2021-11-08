f04a66685af4ff61e30e960e1e5879b2eca4d31c
sang9 <sang9@LAPTOP-2VMJ9RJ9>
2021-11-08 15:19:43 +0900

    implementation UserDAO

--- src/main/java/kr/ac/jbnu/se/awp/sirbay/dao/UserDAO.java
+++ src/main/java/kr/ac/jbnu/se/awp/sirbay/dao/UserDAO.java
@@ -1,5 +1,96 @@
 package kr.ac.jbnu.se.awp.sirbay.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import kr.ac.jbnu.se.awp.sirbay.databaseUtil.DBConnect;
+import kr.ac.jbnu.se.awp.sirbay.dto.UserDTO;
+
 public class UserDAO implements UserDAOIF {
 
+	@Override
+	public ResultSet userSelect(UserDTO userDTO) {
+		String SQL = "SELECT * FROM User WHERE userID = ?";//userID를 통해서만 검색 가능
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBConnect.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userDTO.getUserID());
+			rs = pstmt.executeQuery();
+			return rs;//결과 반환
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.getStackTrace();
+		} finally {
+			try { if(conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if(pstmt != null) pstmt.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if(rs != null) rs.close(); } catch (Exception e) { e.printStackTrace(); }
+		}
+		return null;//DB오류
+	}
+
+	@Override
+	public int userInsert(UserDTO userDTO) {
+		String SQL = "INSERT INTO User VALUES(?,?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBConnect.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userDTO.getUserID());
+			pstmt.setString(2, userDTO.getUserPassword());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.getStackTrace();
+		} finally {
+			try { if(conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if(pstmt != null) pstmt.close(); } catch (Exception e) { e.printStackTrace(); }
+		}
+		return -1;//DB오류
+	}
+
+	@Override
+	public int userUpdate(UserDTO userDTO) {
+		String SQL = "UPDATE User SET userPassword = ?";//userID에는 변경을 가할 수 없도록 함
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBConnect.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userDTO.getUserPassword());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.getStackTrace();
+		} finally {
+			try { if(conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if(pstmt != null) pstmt.close(); } catch (Exception e) { e.printStackTrace(); }
+		}
+		return -1;//DB오류
+	}
+
+	@Override
+	public int userDelete(UserDTO userDTO) {
+		String SQL = "DELETE FROM User WHERE userID = ?";//userID를 통해서만 삭제 가능
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBConnect.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userDTO.getUserID());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.getStackTrace();
+		} finally {
+			try { if(conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+			try { if(pstmt != null) pstmt.close(); } catch (Exception e) { e.printStackTrace(); }
+		}
+		return -1;//DB오류
+	}
+	
 }
--- src/main/java/kr/ac/jbnu/se/awp/sirbay/dao/UserDAOIF.java
+++ src/main/java/kr/ac/jbnu/se/awp/sirbay/dao/UserDAOIF.java
@@ -1,5 +1,12 @@
 package kr.ac.jbnu.se.awp.sirbay.dao;
 
-public interface UserDAOIF {
+import java.sql.ResultSet;
 
+import kr.ac.jbnu.se.awp.sirbay.dto.UserDTO;
+
+public interface UserDAOIF {
+	public ResultSet userSelect(UserDTO userDTO);
+	public int userInsert(UserDTO userDTO);
+	public int userUpdate(UserDTO userDTO);
+	public int userDelete(UserDTO userDTO);
 }
