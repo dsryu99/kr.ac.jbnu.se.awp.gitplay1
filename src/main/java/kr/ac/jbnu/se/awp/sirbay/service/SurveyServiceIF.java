2334bcb1c77d4558f697d76d65531c6c02aa42d3
KHW <hynwoo6901@naver.com>
2021-11-09 16:07:32 +0900

    specify SurveyService interface

--- src/main/java/kr/ac/jbnu/se/awp/sirbay/service/SurveyServiceIF.java
+++ src/main/java/kr/ac/jbnu/se/awp/sirbay/service/SurveyServiceIF.java
@@ -1,5 +1,14 @@
 package kr.ac.jbnu.se.awp.sirbay.service;
 
+import java.util.HashMap;
+import java.util.List;
+
+import kr.ac.jbnu.se.awp.sirbay.dto.QuestionDTO;
+import kr.ac.jbnu.se.awp.sirbay.dto.SurveyDTO;
+
 public interface SurveyServiceIF {
-	
+	boolean addSurvey();
+	List<QuestionDTO> getSurvey(String surveyId);
+	boolean addAnswer(String surveyId, HashMap<Integer, String> answers);
+	List<SurveyDTO> getAllSurveys();
 }
