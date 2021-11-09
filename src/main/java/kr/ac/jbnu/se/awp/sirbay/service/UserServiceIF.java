8eded11512d31282a20eea86b6fd21b21ff5c692
KHW <hynwoo6901@naver.com>
2021-11-09 11:35:17 +0900

    specify UserServiceIF

--- src/main/java/kr/ac/jbnu/se/awp/sirbay/service/UserServiceIF.java
+++ src/main/java/kr/ac/jbnu/se/awp/sirbay/service/UserServiceIF.java
@@ -1,5 +1,12 @@
 package kr.ac.jbnu.se.awp.sirbay.service;
 
+import kr.ac.jbnu.se.awp.sirbay.dto.UserInfoDTO;
+
 public interface UserServiceIF {
-	
+	boolean isUserValid(String id, String password);
+	boolean isUserExist(String id);
+	boolean addUser(String id, String password, String name, int age, String job, String address, int sex);
+	UserInfoDTO getUser(String id);
+	boolean checkPw(String password);
+	boolean deleteUser(String id);
 }
