1ec45f1893bc7e96f11a8ee37c0d84413de197f4
sang981113 <sang981113@naver.com>
2021-11-10 19:14:02 +0900

    DBCPool fix

--- src/main/java/kr/ac/jbnu/se/awp/sirbay/databaseUtil/DBCPool.java
+++ src/main/java/kr/ac/jbnu/se/awp/sirbay/databaseUtil/DBCPool.java
@@ -14,14 +14,13 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 public class DBCPool extends HttpServlet {
 	public void init(ServletConfig config) {
-		String poolName = config.getInitParameter("poolName");        // Pool �씠由�
-		String jdbcDriver = config.getInitParameter("jdbcDriver");    // Driver Class
-		String jdbcURL = config.getInitParameter("jdbcURL");          // MySQL �꽌踰� 二쇱냼
-		String user = config.getInitParameter("user");                // 怨꾩젙 �븘�씠�뵒
-		String password = config.getInitParameter("password");        // 怨꾩젙 鍮꾨�踰덊샇
-		
-		int maxActive = 0;        // 理쒕� �뿰寃곗닔 
-		int maxIdle = 0;          // ��湲곗쨷�씤 �뿰寃곗닔  
+		String poolName = config.getInitParameter("poolName");
+		String jdbcDriver = config.getInitParameter("jdbcDriver");
+		String jdbcURL = config.getInitParameter("jdbcURL");
+		String user = config.getInitParameter("user");
+		String password = config.getInitParameter("password");
+		int maxActive = 10;
+		int maxIdle = 10;
 		
 		try {
 			Class.forName(jdbcDriver);
